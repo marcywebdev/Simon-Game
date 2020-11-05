238e30397e57bdfe99fb2e67a857e44685e4b01a
marcywebdev <marcelinojasso@hotmail.com>
2020-11-05 15:02:21 -0800

    Added animation and play audio file to game.js

--- game.js
+++ game.js
@@ -8,3 +8,8 @@ function nextSequence() {
   gamePattern.push(randomChosenColour);
 }
 
+$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+
