bed2772f78dc5a52b5c92ad708fad156092d1366
marcywebdev <marcelinojasso@hotmail.com>
2020-11-09 16:16:29 -0800

    fixed audio issues not registering to game.js

--- game.js
+++ game.js
@@ -6,19 +6,23 @@ var userClickedPattern = [];
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
 });
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
 }
 
+function playSound(name) {
 
-$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 
-  
+}
 
